9a31b5c848bc8278a7c840482b14cd7ffe2775f4
Sibi Vishtan Thirukonda <thirukonda.s@northeastern.edu>
2024-07-07 21:10:27 -0400

    number of islands

--- practice.cpp
+++ practice.cpp
@@ -6,79 +6,34 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        unordered_map<char, vector<pair<int, int>>> mp;
-        int M = board.size();
-        int N = board[0].size();
-        int totalCells = M*N;
-        for(int i = 0; i < M; i++){
-            for(int j = 0; j < N; j++){
-                mp[board[i][j]].push_back({i,j});
-            }
+    int numIslands(std::vector<std::vector<char>>& grid ){
+        if(grid.empty()){
+            return 0;
         }
-        int startWithA = 0, endWithA = 0;
-        for (auto word : words){
-            if (word.size() > totalCells){
-                continue;
-            }
-			if (word.front() == 'a'){
-				startWithA++;
-            }
-			if (word.back() == 'a'){
-				endWithA++;
-            }
-		}
-        bool reverseSearch = false;
-        if(startWithA > endWithA){
-            reverseSearch = true;
-        }
-        vector<string> result;
-        bool canFound;
-        string wordCopy;
-        for(auto word : words){
-            vector<int> temp(26, 0);
-            canFound = true;
-            for(auto ch : word){
-                temp[ch-'a']++;
-                if(temp[ch-'a'] > mp[ch].size()){
-                    canFound = false;
-                    break;
-                }
-            }
-            if(!canFound){
-                continue;
-            }
-            wordCopy = word;
-            if(reverseSearch){
-                reverse(wordCopy.begin(), wordCopy.end());
-            }
-            for(auto cell : mp[wordCopy[0]]){
-                if(processCell(board, cell.first, cell.second, M, N, wordCopy, 0)){
-                    result.push_back(word);
-                    break;
+        int num_islands = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for (int i= 0; i<rows; ++i){
+            for(int j=0; j<cols; ++j){
+                if(grid[i][j] == '1'){
+                    ++num_islands;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return result;
+        return num_islands;
     }
-private:
-    bool processCell(vector<vector<char>>& board, int i, int j, int M, int N, const string &word, int index){
-        if(index == word.size()){
-            return true;
+    private:
+        void dfs(std::vector<std::vector<char>>& grid, int i, int j){
+            if(i<0 ||j<0||i>=grid.size()||grid[i][j] == '0'){
+                return;
+            }
+            grid[i][j] = '0';
+            dfs(grid, i-1, j);
+            dfs(grid, i+1, j);
+            dfs(grid, i, j-1);
+            dfs(grid, i, j+1);
         }
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size()){
-            return false;
-        }
-        char ch = board[i][j];
-        if(board[i][j] != word[index]){
-            return false;
-        }
-        board[i][j] = '#';
-        bool result = processCell(board, i+1, j, M, N, word, index+1) ||
-        processCell(board, i, j+1, M, N, word, index+1) ||
-        processCell(board, i-1, j, M, N, word, index+1) ||
-        processCell(board, i, j-1, M, N, word, index+1);
-        board[i][j] = ch;
-        return result;
-    }
+
 };
