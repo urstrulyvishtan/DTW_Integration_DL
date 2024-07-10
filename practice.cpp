a0a1218506872809d8f08836e3730bc3c672a69f
Sibi Vishtan Thirukonda <thirukonda.s@northeastern.edu>
2024-07-10 00:59:10 -0400

    LCS

--- practice.cpp
+++ practice.cpp
@@ -6,13 +6,20 @@
 #include<unordered_set>
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<int> dp(n, 1);
-         for(int i = 1; i<m; ++i){
-            for(int j = 1; j<n; ++j){
-                dp[j] += dp[j-1];
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i = 1; i<=m; ++i){
+                for(int j = 1; j<=n; ++j){
+                    if(text1[i-1] == text2[j-1]){
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    }
+                    else{
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j-1]);
+                    }
+                }
             }
-         }
-         return dp[n-1];
-    }
+            return dp[m][n];
+        }
 };
