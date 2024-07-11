2ab16b3f8fe1588c43b472626a48454c60fc898a
Sibi Vishtan <sibivishtantt@gmail.com>
2024-07-10 23:00:23 -0400

    max subarray

--- practice.cpp
+++ practice.cpp
@@ -4,22 +4,79 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
+int speedUp = [] {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    return 0;
+}();
+
+// Function to extract digit value from a character
+int digit(char c) {
+    return c & 15;
+}
+
+// Function to check if a character is a digit
+bool isDigit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+int in = [] {
+    std::ofstream out("user.out"); // Redirect cout to user.out
+    std::cout.rdbuf(out.rdbuf());
+
+    int ans, sum;
+    bool _neg;
+    
+    // Read input line by line
+    for (std::string s; std::getline(std::cin, s); std::cout << '\n') {
+        ans = -1e9, sum = 0; // Initialize answer and sum for each input line
+        
+        for (int _i = 1, _n = s.length(), v; _i < _n; ++_i) {
+            _neg = false; // Initialize flag to indicate negative number
+            
+            if (s[_i] == '-') { // Check for negative sign
+                ++_i; // Move to next character after '-'
+                _neg = true; // Set flag to true for negative number
+            }
+            
+            v = digit(s[_i]); // Extract first digit
+            
+            // Extract remaining digits to form the number
+            while (isDigit(s[++_i]))
+                v = v * 10 + digit(s[_i]);
+            
+            // Negate the value if it was negative
+            if (_neg)
+                v = -v;
+            
+            // Do stuff with v (not specified in the provided code)
+            
+            // Update answer by taking maximum of current answer and sum
+            ans = std::max(ans, sum < 0 ? sum = v : sum += v);
+        }
+        
+        // Output the answer
+        std::cout << ans;
+    }
+    
+    // Exit after processing input
+    exit(0);
+    return 0;
+}();
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size();
-        int n = text2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        for(int i = 1; i<=m; ++i){
-                for(int j = 1; j<=n; ++j){
-                    if(text1[i-1] == text2[j-1]){
-                        dp[i][j] = dp[i-1][j-1] + 1;
-                    }
-                    else{
-                        dp[i][j] = max(dp[i - 1][j], dp[i][j-1]);
-                    }
-                }
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxx=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum>maxx){
+                maxx=sum;
             }
-            return dp[m][n];
+            if(sum<0){
+                sum=0;
+            }
         }
+        return maxx;
+    }
 };
