1425748fdb4829020a05ed613a60c6af806d04fb
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-17 20:15:20 -0400

    sliding window

--- practice.cpp
+++ practice.cpp
@@ -6,42 +6,23 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        if(s1.size() > s2.size()) return false;
-        vector<int> s1Count(26, 0), s2Count(26, 0);
-        for(char c: s1){
-            s1Count[c - 'a']++;
-        }
-        for(int i = 0; i<s1.size(); i++){
-            s2Count[s2[i] - 'a']++;
-        }
-        int matches = 0;
-        for(int i=0; i<26; i++){
-            if(s1Count[i]==s2Count[i]){
-            matches++;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> deq;
+        vector<int> result;
+
+        for(int i = 0; i<nums.size(); ++i){
+            if(!deq.empty() && deq.front() == i - k){
+                deq.pop_front();
+            }
+            while(!deq.empty() && nums[deq.back()]<=nums[i]){
+                deq.pop_back();
+            }
+        deq.push_back(i);
+        if(i>=k-1){
+            result.push_back(nums[deq.front()]);
         }
     }
-
-    int left = 0;
-    for(int right = s1.size(); right<s2.size(); right++){
-        if(matches == 26) return true;
-
-        int index = s2[right] - 'a';
-        s2Count[index]++;
-        if(s1Count[index] == s2Count[index]){
-            matches++;
-        }else if(s1Count[index] + 1 == s2Count[index]){
-            matches--;
-        }
-        index = s2[left] - 'a';
-        s2Count[index]--;
-        if(s1Count[index] == s2Count[index]){
-            matches++;
-        }else if(s1Count[index] - 1 == s2Count[index]){
-            matches--;
-        }
-        left++;
-    }
-    return matches == 26;
+    return result;
 }
+        
 };
