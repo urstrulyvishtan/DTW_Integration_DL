9c8515681d2bd6c0c5a21ab10fb11d799e7387e5
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-19 12:00:36 -0400

    create test_torch.py

---
+++ PyDTW/test_torch.py
@@ -0,0 +1,12 @@
+import torch
+import unittest
+
+class TestDTW(unittest.TestCase):
+    def test_dtw_forward(self):
+
+        pass
+    def test_dtw_backward(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
