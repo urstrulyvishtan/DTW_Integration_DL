1a2477d1e42c444ecd84068f5c9b907b98afc195
Sibi Vishtan Thirukonda <thirukonda.s@northeastern.edu>
2024-07-21 20:54:48 -0400

    stack operations

--- practice.cpp
+++ practice.cpp
@@ -4,25 +4,47 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-class Solution {
+class MinStack {
+    private:
+        stack<int> mainStack;
+        stack<int> minStack;
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> deq;
-        vector<int> result;
-
-        for(int i = 0; i<nums.size(); ++i){
-            if(!deq.empty() && deq.front() == i - k){
-                deq.pop_front();
-            }
-            while(!deq.empty() && nums[deq.back()]<=nums[i]){
-                deq.pop_back();
-            }
-        deq.push_back(i);
-        if(i>=k-1){
-            result.push_back(nums[deq.front()]);
+    MinStack() {
+        minStack.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+        if(val<=minStack.top()){
+            minStack.push(val);
         }
     }
-    return result;
-}
-        
+    
+    void pop() {
+        if(mainStack.empty()) return;
+        int top = mainStack.top();
+        mainStack.pop();
+        if(top == minStack.top()){
+            minStack.pop();
+        }
+    }
+    
+    int top() {
+        if(mainStack.empty()) throw runtime_error("Stack is empty");
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        if(minStack.empty()) throw runtime_error("Stack is empty");
+        return minStack.top();
+    }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
