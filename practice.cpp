2971177ee98bb1e29e9ed7ad7a1f43bbd71d2943
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-22 23:14:55 -0400

    car fleet

--- practice.cpp
+++ practice.cpp
@@ -6,20 +6,24 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> result(n, 0);
-        stack<int> stk;
-
-        for (int i = 0; i < n; ++i) {
-            while (!stk.empty() && temperatures[i] > temperatures[stk.top()]) {
-                int idx = stk.top();
-                stk.pop();
-                result[idx] = i - idx;
-            }
-            stk.push(i);
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<pair<double, int>> cars(n);
+        for(int i = 0; i<n; ++i){
+            double time = (double)(target-position[i]) / speed[i];
+            cars[i] = {time, position[i]};
         }
-
-        return result;
+        sort(cars.begin(), cars.end(), [](const pair<double, int>& a, const pair<double, int>& b){
+            return a.second>b.second;
+        });
+        int fleets = 0;
+        double currentFleetTime = 0;
+        for(const auto& car:cars){
+            if(car.first>currentFleetTime){
+                ++fleets;
+                currentFleetTime = car.first;
+            }
+        }
+        return fleets;
     }
 };
