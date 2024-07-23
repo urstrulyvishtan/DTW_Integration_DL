cd43887f4d3820f2cc118a854cd6ee93b750bc76
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-22 23:08:07 -0400

    daily temperatures

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,20 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        generateParanthesisHelper(result, "", 0, 0, n);
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> result(n, 0);
+        stack<int> stk;
+
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && temperatures[i] > temperatures[stk.top()]) {
+                int idx = stk.top();
+                stk.pop();
+                result[idx] = i - idx;
+            }
+            stk.push(i);
+        }
+
         return result;
-    }
-private:
-    void generateParanthesisHelper(vector<string>& result, string current, int open, int close, int n){
-        if(current.length() ==  2 * n){
-            result.push_back(current);
-            return;
-        }
-        if(open<n){
-            generateParanthesisHelper(result, current + '(', open + 1, close, n);
-        }
-        if(close<open){
-            generateParanthesisHelper(result, current +')', open, close+1, n);
-        }
     }
 };
