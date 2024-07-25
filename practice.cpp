dcee01240573df124d7a5be279370d62223df7ff
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-25 00:07:34 -0400

    koko eating bananas

--- practice.cpp
+++ practice.cpp
@@ -6,23 +6,24 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty() || matrix[0].empty()) return false;
-
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int low = 0, high = m*n-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            int midValue = matrix[mid/n][mid%n];
-            if(midValue == target){
-                return true;
-            } else if(midValue<target){
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(canFinish(piles, mid, h)){
+                high = mid;
+            } else{
                 low = mid+1;
-            }else{
-                high = mid-1;
             }
         }
-        return false;
+        return low;
+    }
+    bool canFinish(const vector<int>& piles, int k, int h){
+        int hours = 0;
+        for(int pile:piles){
+            hours+=ceil((double)pile/k);
+        }
+        return hours<=h;
     }
 };
