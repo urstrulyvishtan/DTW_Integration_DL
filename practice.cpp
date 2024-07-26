6ee1b6ee8ead80cac79ddd7bd193da6972eb8e7c
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-26 01:55:19 -0400

    find the duplicate number

--- practice.cpp
+++ practice.cpp
@@ -4,35 +4,20 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* p = l1, *q = l2, *current = dummyHead;
-        int carry = 0;
-        while(p != nullptr||q!=nullptr){
-            int x = (p!=nullptr)?p->val:0;
-            int y = (q!=nullptr)?q->val:0;
-            int sum = carry + x + y;
-            carry = sum/10;
-            current->next = new ListNode(sum%10);
-            current = current->next;
-            if(p!=nullptr) p = p->next;
-            if(q!=nullptr) q = q->next;
+    int findDuplicate(vector<int>& nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do{
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        }while(tortoise!=hare);
+        tortoise = nums[0];
+        while(tortoise != hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        if(carry>0){
-            current->next = new ListNode(carry);
-        }
-        return dummyHead->next;
+        return hare;
     }
 };
