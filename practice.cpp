e9b3c79ac86c63af95354cfd78a6f2b01c408254
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-28 23:54:00 -0400

    design twitter

--- practice.cpp
+++ practice.cpp
@@ -4,34 +4,63 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-class Solution {
+class Twitter {
+private:
+struct Tweet{
+    int id;
+    int timestamp;
+    Tweet(int tweetId, int time) : id(tweetId), timestamp(time){}
+};
+int time;
+unordered_map<int, vector<Tweet>> userTweets;
+unordered_map<int, unordered_set<int>> followers;
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> taskCounts;
-        for(char task:tasks){
-            taskCounts[task]++;
+    Twitter() : time(0){
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        userTweets[userId].emplace_back(tweetId, time++);
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> result;
+        priority_queue<pair<int, int>> maxHeap;
+        for(auto& tweet : userTweets[userId]){
+            maxHeap.push({tweet.timestamp, tweet.id});
         }
-        priority_queue<int> maxHeap;
-        for(auto& pair : taskCounts){
-            maxHeap.push(pair.second);
-        }
-        int time = 0;
-        queue<pair<int, int>> cooldownQueue;
-        while(!maxHeap.empty() || !cooldownQueue.empty()){
-            time++;
-            if(!maxHeap.empty()){
-                int currentTaskCount = maxHeap.top();
-                maxHeap.pop();
-                currentTaskCount--;
-                if(currentTaskCount>0){
-                    cooldownQueue.push({currentTaskCount, time+n});
-                }
-            }
-            if(!cooldownQueue.empty() && cooldownQueue.front().second == time){
-                maxHeap.push(cooldownQueue.front().first);
-                cooldownQueue.pop();
+        for(auto& followeeId : followers[userId]){
+            for(auto& tweet : userTweets[followeeId]){
+                maxHeap.push({tweet.timestamp, tweet.id});
             }
         }
-        return time;
+        int count = 0;
+        while(!maxHeap.empty() && count<10){
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
+            count++;
+        }
+        return result;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        if(followerId != followeeId){
+            followers[followerId].insert(followeeId);
+        }
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(followerId != followeeId){
+            followers[followerId].erase(followeeId);
+        }   
     }
 };
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
