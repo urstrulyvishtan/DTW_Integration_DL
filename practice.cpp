4d3c1d2ca134ef6d14c3dea2c2c8861d2e2b69a4
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-28 23:28:58 -0400

    kth largest element in Stream

--- practice.cpp
+++ practice.cpp
@@ -4,54 +4,32 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
- ListNode* reverse(ListNode* start, ListNode* end){
-    ListNode* prev = nullptr;
-    ListNode* curr = start;
-    ListNode* next = nullptr;
-    while(curr!=end){
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
- }
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class KthLargest {
 public:
-    int goodNodes(TreeNode* root) {
-        return countGoodNodes(root, INT_MIN);
+    KthLargest(int k, vector<int>& nums) : k(k){
+        for(int num : nums){
+            add(num);
+        }
+    }
+    
+    int add(int val) {
+        if(minHeap.size() < k){
+            minHeap.push(val);
+        } else if(val>minHeap.top()){
+            minHeap.push(val);
+            if(minHeap.size()>k){
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
     }
 private:
-    int countGoodNodes(TreeNode* node, int maxSoFar){
-        if(!node) return 0;
-        int count = 0;
-        if(node->val >= maxSoFar){
-            count = 1;
-            maxSoFar = node->val;
-        }
-        count += countGoodNodes(node->left, maxSoFar);
-        count += countGoodNodes(node->right, maxSoFar);
-        return count;
-    }
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    int k;
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
