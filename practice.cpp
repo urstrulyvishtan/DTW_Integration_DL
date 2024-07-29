b73591a0b2d715a892e7098562acaca126ddf862
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-07-28 23:44:59 -0400

    task scheduler

--- practice.cpp
+++ practice.cpp
@@ -6,14 +6,32 @@
 #include<unordered_set>
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
-        for(int num : nums){
-            minHeap.push(num);
-            if(minHeap.size()>k){
-                minHeap.pop();
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> taskCounts;
+        for(char task:tasks){
+            taskCounts[task]++;
+        }
+        priority_queue<int> maxHeap;
+        for(auto& pair : taskCounts){
+            maxHeap.push(pair.second);
+        }
+        int time = 0;
+        queue<pair<int, int>> cooldownQueue;
+        while(!maxHeap.empty() || !cooldownQueue.empty()){
+            time++;
+            if(!maxHeap.empty()){
+                int currentTaskCount = maxHeap.top();
+                maxHeap.pop();
+                currentTaskCount--;
+                if(currentTaskCount>0){
+                    cooldownQueue.push({currentTaskCount, time+n});
+                }
+            }
+            if(!cooldownQueue.empty() && cooldownQueue.front().second == time){
+                maxHeap.push(cooldownQueue.front().first);
+                cooldownQueue.pop();
             }
         }
-        return minHeap.top();
+        return time;
     }
 };
