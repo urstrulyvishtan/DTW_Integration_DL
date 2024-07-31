aaae44d9496f309b398f283737af6e970758c650
Sibi Vishtan Thirukonda <thirukonda.s@northeastern.edu>
2024-07-30 21:50:53 -0400

    merge intervals

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,22 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if(intervals.empty()){
-            return {};        }
-    
-    sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
-        return a[0]<b[0];
-    });
-    vector<vector<int>> mergedIntervals;
-    mergedIntervals.push_back(intervals[0]);
-    for(const auto& interval:intervals){
-        if(interval[0]<=mergedIntervals.back()[1]){
-            mergedIntervals.back()[1] = max(mergedIntervals.back()[1], interval[1]);
-        }else{
-            mergedIntervals.push_back(interval);
+            return 0;
         }
-    }
-    return mergedIntervals;
+        sort(intervals.begin(), intervals.end(),[](const vector<int>& a, vector<int>& b){
+            return a[1]<b[1];
+        });
+        int count = 0;
+        int lastEnd = intervals[0][1];
+        for(int i = 1; i<intervals.size(); ++i){
+            if(intervals[i][0] < lastEnd){
+                ++count;
+            }else{
+                lastEnd = intervals[i][1];
+            }
+        }
+        return count;
     }
 };
