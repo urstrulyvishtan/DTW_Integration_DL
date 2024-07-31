677abc9b9a3379b3e071be4ddb00bfd335a79460
Sibi Vishtan Thirukonda <thirukonda.s@northeastern.edu>
2024-07-30 21:56:12 -0400

    meeting schedule

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,19 @@
 #include<unordered_set>
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if(intervals.empty()){
-            return 0;
-        }
-        sort(intervals.begin(), intervals.end(),[](const vector<int>& a, vector<int>& b){
-            return a[1]<b[1];
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        // Sort the intervals based on their start times
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) {
+            return a.start < b.start;
         });
-        int count = 0;
-        int lastEnd = intervals[0][1];
-        for(int i = 1; i<intervals.size(); ++i){
-            if(intervals[i][0] < lastEnd){
-                ++count;
-            }else{
-                lastEnd = intervals[i][1];
+
+        // Check for overlapping intervals
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
             }
         }
-        return count;
+
+        return true;
     }
 };
