b1455a3ed31e589d53985fea5dd5bfa16d1016ff
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-02 10:35:15 -0400

    spiral matrix

--- practice.cpp
+++ practice.cpp
@@ -6,23 +6,46 @@
 #include<unordered_set>
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for(int i = 0; i<n; ++i){
-            for(int j = i+1; j<n; ++j){
-                swap(matrix[i][j], matrix[j][i]);
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        
+        if (matrix.empty()) return result;
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0, right = n - 1;
+        int top = 0, bottom = m - 1;
+
+        while (left <= right && top <= bottom) {
+            // Traverse from left to right along the top boundary
+            for (int i = left; i <= right; ++i) {
+                result.push_back(matrix[top][i]);
+            }
+            top++;
+            
+            // Traverse from top to bottom along the right boundary
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            // Traverse from right to left along the bottom boundary
+            if (top <= bottom) {
+                for (int i = right; i >= left; --i) {
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            // Traverse from bottom to top along the left boundary
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
             }
         }
-        for(int i = 0; i<n; ++i){
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-    }
-    void printMatrix(const vector<vector<int>>& matrix){
-        for(const auto& row:matrix){
-            for(int val:row){
-                cout<<val<<" ";
-            }
-            cout<<endl;
-        }
+        
+        return result;
     }
 };
