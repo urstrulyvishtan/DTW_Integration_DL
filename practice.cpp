4786839ab12f821776888f3f670f38b5518d7ab3
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-03 16:33:12 -0400

    count 1 bits

--- practice.cpp
+++ practice.cpp
@@ -6,58 +6,12 @@
 #include<unordered_set>
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        bool firstRowZero = false, firstColZero = false;
-
-        // Check if the first row needs to be zeroed
-        for (int j = 0; j < n; ++j) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
-            }
+    int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0){
+            count+=n&1;
+            n>>=1;
         }
-
-        // Check if the first column needs to be zeroed
-        for (int i = 0; i < m; ++i) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
-            }
-        }
-
-        // Use the first row and column to mark zeros
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-
-        // Zero out cells based on the markers in the first row and column
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Zero out the first row if needed
-        if (firstRowZero) {
-            for (int j = 0; j < n; ++j) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        // Zero out the first column if needed
-        if (firstColZero) {
-            for (int i = 0; i < m; ++i) {
-                matrix[i][0] = 0;
-            }
-        }
+        return count;
     }
 };
