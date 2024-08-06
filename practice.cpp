d0331b32b12832a0c642aed4f20ce63b781ff87f
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-06 14:17:16 -0400

    combination sum 2

--- practice.cpp
+++ practice.cpp
@@ -6,21 +6,25 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> result;
-        vector<int> subset;
-        sort(nums.begin(), nums.end());
-        backtrack(nums, 0, subset, result);
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, target, 0, combination, result);
         return result;
     }
 private:
-    void backtrack(vector<int>& nums, int start, vector<int>& subset, vector<vector<int>>& result){
-        result.push_back(subset);
-        for(int i = start; i<nums.size(); ++i){
-            if(i>start && nums[i]==nums[i-1]) continue;
-            subset.push_back(nums[i]);
-            backtrack(nums, i+1, subset, result);
-            subset.pop_back();
+    void backtrack(vector<int>& candidates, int target, int start, vector<int>& combination, vector<vector<int>>& result){
+        if(target == 0){
+            result.push_back(combination);
+            return;
+        }
+        for(int i = start; i<candidates.size(); ++i){
+            if(i>start && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            combination.push_back(candidates[i]);
+            backtrack(candidates, target-candidates[i], i+1, combination, result);
+            combination.pop_back();
         }
     }
 };
