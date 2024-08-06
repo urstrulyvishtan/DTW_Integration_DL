75d27b42335f0910ce4602a1cba6281cf3171929
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-06 14:34:39 -0400

    letter combination

--- practice.cpp
+++ practice.cpp
@@ -6,34 +6,30 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> currentPartition;
-        backtrack(s, 0, currentPartition, result);
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        vector<string> result;
+        string currentCombination;
+        unordered_map<char, string> digitToChar = {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+            {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+            {'8', "tuv"}, {'9', "wxyz"}
+        };
+        backtrack(digits, 0, currentCombination, result, digitToChar);
         return result;
     }
 private:
-    void backtrack(const string& s, int start, vector<string>& currentPartition, vector<vector<string>>& result){
-        if(start == s.length()){
-            result.push_back(currentPartition);
+    void backtrack(const string& digits, int index, string& currentCombination, vector<string>& result, const unordered_map<char, string>& digitToChar){
+        if(index == digits.size()){
+            result.push_back(currentCombination);
             return;
         }
-        for(int end = start; end<s.length(); ++end){
-            if(isPalindrome(s, start, end)){
-            currentPartition.push_back(s.substr(start, end-start+1));
-            backtrack(s, end+1, currentPartition, result);
-            currentPartition.pop_back();
+        char digit = digits[index];
+        const string& letters = digitToChar.at(digit);
+        for(char letter:letters){
+            currentCombination.push_back(letter);
+            backtrack(digits, index+1, currentCombination, result, digitToChar);
+            currentCombination.pop_back();
         }
-        }
-    }
-    bool isPalindrome(const string& s, int left, int right){
-        while(left<right){
-            if(s[left]!=s[right]){
-                return false;
-            }
-            ++left;
-            --right;
-        }
-        return true;
     }
 };
