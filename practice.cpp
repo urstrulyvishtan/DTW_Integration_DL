6033a9b86cb57d8d143bc98b6b03770eb0888150
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-06 13:58:43 -0400

    subset

--- practice.cpp
+++ practice.cpp
@@ -6,50 +6,19 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-         // Sort intervals by start time
-        sort(intervals.begin(), intervals.end());
-        
-        // Create a vector of pairs to store queries and their original indices
-        vector<pair<int, int>> queriesWithIndices;
-        for (int i = 0; i < queries.size(); ++i) {
-            queriesWithIndices.push_back({queries[i], i});
-        }
-        
-        // Sort queries by value
-        sort(queriesWithIndices.begin(), queriesWithIndices.end());
-        
-        // Min-heap to store intervals by size
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        
-        vector<int> result(queries.size(), -1);
-        int i = 0;
-        
-        // Iterate over each query
-        for (auto& query : queriesWithIndices) {
-            int q = query.first;
-            int idx = query.second;
-            
-            // Add all intervals that start before or at the current query point
-            while (i < intervals.size() && intervals[i][0] <= q) {
-                int start = intervals[i][0];
-                int end = intervals[i][1];
-                int size = end - start + 1;
-                minHeap.push({size, end});
-                ++i;
-            }
-            
-            // Remove all intervals from the heap that end before the current query point
-            while (!minHeap.empty() && minHeap.top().second < q) {
-                minHeap.pop();
-            }
-            
-            // If the heap is not empty, the top element is the smallest interval containing the query
-            if (!minHeap.empty()) {
-                result[idx] = minHeap.top().first;
-            }
-        }
-        
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> subset;
+        backtrack(nums, 0, subset, result);
         return result;
+    }
+private:
+    void backtrack(vector<int>& nums, int start, vector<int>& subset, vector<vector<int>>& result){
+        result.push_back(subset);
+        for(int i = start; i<nums.size(); ++i){
+            subset.push_back(nums[i]);
+            backtrack(nums, i+1, subset, result);
+            subset.pop_back();
+        }
     }
 };
