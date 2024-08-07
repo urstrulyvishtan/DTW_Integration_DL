90725a7649a992db7c2318cb90220810f6f8f5a6
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-07 19:25:00 -0400

    surrounded regions

--- practice.cpp
+++ practice.cpp
@@ -6,47 +6,45 @@
 #include<unordered_set>
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int, int>> q;
-        int freshOranges = 0;
-        for(int i = 0; i<m; ++i){
-            for(int j = 0; j<n; ++j){
-                if(grid[i][j] == 2){
-                    q.push({i, j});
-                }else if (grid[i][j] == 1){
-                    freshOranges++;
-                }
-            }
+    void solve(vector<vector<char>>& board) {
+        if (board.empty()) return;
+        int m = board.size();
+        int n = board[0].size();
+        
+        // Marking escape regions
+        for (int i = 0; i < m; ++i) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
         }
-        if(freshOranges == 0) return 0;
-        vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int minutes = 0;
-        while(!q.empty()){
-            int size = q.size();
-            bool rottedAny = false;
-            for(int i = 0; i<size; ++i){
-                auto[row, col] = q.front();
-                q.pop();
-                for(const auto& direction:directions){
-                    int newRow = row+direction[0];
-                    int newCol = col+direction[1];
-                    if(newRow<0||newRow>=m||newCol<0||newCol>=n||grid[newRow][newCol] != 1){
-                        continue;
-                    }
-                    grid[newRow][newCol] = 2;
-                    q.push({newRow, newCol});
-                    freshOranges--;
-                    rottedAny = true;
-                }
-            }
-            if(rottedAny){
-                minutes++;
-            }
+        for (int j = 0; j < n; ++j) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
 
+        // Flipping captured regions and reverting marks
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'E') {
+                    board[i][j] = 'O';
+                }
+            }
         }
-        return freshOranges == 0?minutes : -1;
+    }
+
+private:
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        int m = board.size();
+        int n = board[0].size();
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
+
+        board[i][j] = 'E'; // Mark as escape
+        vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (const auto& dir : directions) {
+            int newI = i + dir[0];
+            int newJ = j + dir[1];
+            dfs(board, newI, newJ);
+        }
     }
 };
