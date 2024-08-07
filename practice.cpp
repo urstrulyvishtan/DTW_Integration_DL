d1aed77ae85c6dc94dcd966d2d53e8847ea0b253
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-07 19:31:13 -0400

    course schedule

--- practice.cpp
+++ practice.cpp
@@ -6,45 +6,36 @@
 #include<unordered_set>
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        if (board.empty()) return;
-        int m = board.size();
-        int n = board[0].size();
-        
-        // Marking escape regions
-        for (int i = 0; i < m; ++i) {
-            if (board[i][0] == 'O') dfs(board, i, 0);
-            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> inDegree(numCourses, 0);
+        for(auto& pre:prerequisites){
+            int course = pre[0];
+            int prerequisite = pre[1];
+            graph[prerequisite].push_back(course);
+            inDegree[course]++;
         }
-        for (int j = 0; j < n; ++j) {
-            if (board[0][j] == 'O') dfs(board, 0, j);
-            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        queue<int> q;
+        for(int i = 0; i<numCourses; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
         }
-
-        // Flipping captured regions and reverting marks
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                } else if (board[i][j] == 'E') {
-                    board[i][j] = 'O';
+        vector<int> order;
+        while(!q.empty()){
+            int current = q.front();
+            q.pop();
+            order.push_back(current);
+            for(int neighbor : graph[current]){
+                inDegree[neighbor]--;
+                if(inDegree[neighbor] == 0){
+                    q.push(neighbor);
                 }
             }
         }
-    }
-
-private:
-    void dfs(vector<vector<char>>& board, int i, int j) {
-        int m = board.size();
-        int n = board[0].size();
-        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
-
-        board[i][j] = 'E'; // Mark as escape
-        vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        for (const auto& dir : directions) {
-            int newI = i + dir[0];
-            int newJ = j + dir[1];
-            dfs(board, newI, newJ);
-        }
+        if(order.size() == numCourses){
+            return order;
+        } else{
+            return {};        }
     }
 };
