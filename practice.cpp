91b5b4d41a7372460d5bcb79c7e91c3f28bce04a
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-07 19:38:41 -0400

    redundant connection

--- practice.cpp
+++ practice.cpp
@@ -4,38 +4,54 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
+class UnionFind{
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    UnionFind(int size){
+        parent.resize(size);
+        rank.resize(size, 0);
+        for(int i = 0; i<size; ++i){
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if(parent[x]!=x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    bool unionSets(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX == rootY){
+            return false;
+        }
+        if(rank[rootX]>rank[rootY]){
+            parent[rootY] = rootX;
+        } else if(rank[rootX] < rank[rootY]){
+            parent[rootX] = rootY;
+        } else{
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+        return true;
+    }
+
+};
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> inDegree(numCourses, 0);
-        for(auto& pre:prerequisites){
-            int course = pre[0];
-            int prerequisite = pre[1];
-            graph[prerequisite].push_back(course);
-            inDegree[course]++;
-        }
-        queue<int> q;
-        for(int i = 0; i<numCourses; i++){
-            if(inDegree[i] == 0){
-                q.push(i);
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        UnionFind uf(n+1);
+        for(const auto& edge:edges){
+            if(!uf.unionSets(edge[0], edge[1])){
+                return edge;
             }
         }
-        vector<int> order;
-        while(!q.empty()){
-            int current = q.front();
-            q.pop();
-            order.push_back(current);
-            for(int neighbor : graph[current]){
-                inDegree[neighbor]--;
-                if(inDegree[neighbor] == 0){
-                    q.push(neighbor);
-                }
-            }
-        }
-        if(order.size() == numCourses){
-            return order;
-        } else{
-            return {};        }
+        return{};
     }
 };
+
