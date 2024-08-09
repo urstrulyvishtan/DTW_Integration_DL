051534854234070b15d36c18452066dd809c81f4
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-09 11:49:23 -0400

    plus oneq

--- practice.cpp
+++ practice.cpp
@@ -6,25 +6,16 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool isHappy(int n) {
-        std::unordered_set<int> seen;
-        
-        while (n != 1 && seen.find(n) == seen.end()) {
-            seen.insert(n);
-            n = getNext(n);
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for(int i = n-1; i>=0; --i){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        
-        return n == 1;
-    }
-    
-private:
-    int getNext(int n) {
-        int totalSum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            totalSum += digit * digit;
-            n /= 10;
-        }
-        return totalSum;
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
