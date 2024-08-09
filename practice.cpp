026fca9c2777fd9a4796d13bd4e27686b17793ad
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-09 11:52:24 -0400

    pow(x,y)

--- practice.cpp
+++ practice.cpp
@@ -6,16 +6,24 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        for(int i = n-1; i>=0; --i){
-            if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0){
+            x = 1/x;
+            N = -N;
         }
-        digits.insert(digits.begin(), 1);
-        return digits;
+        return fastPow(x, N);
+    }
+private:
+    double fastPow(double x, long long n){
+        if(n==0){
+            return 1.0;
+        }
+        double half = fastPow(x, n/2);
+        if(n%2 == 0){
+            return half*half;
+        } else{
+            return half*half*x;
+        }
     }
 };
