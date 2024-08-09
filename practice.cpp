cebeee345546616f6f55c9e0423137c74b59e637
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-09 11:57:17 -0400

    multiply

--- practice.cpp
+++ practice.cpp
@@ -6,24 +6,24 @@
 #include<unordered_set>
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long long N = n;
-        if(N<0){
-            x = 1/x;
-            N = -N;
+    string multiply(string num1, string num2) {
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> result(m+n, 0);
+        for(int i = m-1; i>=0; --i){
+            for(int j = n-1; j>=0; --j){
+                int mul = (num1[i] - '0')*(num2[j] - '0');
+                int sum = mul+result[i+j+1];
+                result[i+j+1] = sum%10;
+                result[i+j] += sum/10;
+            }
         }
-        return fastPow(x, N);
-    }
-private:
-    double fastPow(double x, long long n){
-        if(n==0){
-            return 1.0;
+        string product = "";
+        for(int num:result){
+            if(!(product.empty() && num == 0)){
+                product.push_back(num + '0');
+            }
         }
-        double half = fastPow(x, n/2);
-        if(n%2 == 0){
-            return half*half;
-        } else{
-            return half*half*x;
-        }
+        return product.empty() ? "0" : product;
     }
 };
