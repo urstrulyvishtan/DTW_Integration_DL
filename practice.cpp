9ef384919d0ced5548724bacaebc80acab503487
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-10 19:39:54 -0400

    cheapest flights stops k
    `

--- practice.cpp
+++ practice.cpp
@@ -6,31 +6,40 @@
 #include<unordered_set>
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<pair<int, int>>> graph;
-        for(const auto& flight:flights){
-            int u = flight[0], v = flight[1], w = flight[2];
-            graph[u].emplace_back(v, w);
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        vector<pair<int,int>>adj[n];
+        for(auto it:flights)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        pq.emplace(0, src, 0);
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-        while(!pq.empty()){
-            auto[cost, city, stops] = pq.top(); pq.pop();
-            if(city == dst){
-                return cost;
-            }
-            if(stops <= k){
-                for(const auto& [next_city, price]:graph[city]){
-                    int new_cost = cost+price;
-                    if(new_cost<dist[next_city]||stops+1 <=k){
-                        dist[next_city] = new_cost;
-                        pq.emplace(new_cost, next_city, stops+1);
-                    }
+        queue<pair<int,pair<int,int>>>q;
+            // {stops, {node, distance}}
+        q.push({0,{src,0}});
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            int stops=it.first;
+            int node=it.second.first;
+            int weight=it.second.second;
+            if(stops>K)
+                continue;
+            for(auto i:adj[node])
+            {
+                int adjNode=i.first;
+                int adjWt=i.second;
+                if(weight+adjWt<dist[adjNode])
+                {
+                    q.push({stops+1,{adjNode,weight+adjWt}});
+                    dist[adjNode]=weight+adjWt;
                 }
             }
         }
-        return -1;
+            // destination do not reached under k stops
+        if(dist[dst]==INT_MAX)
+            return -1;
+        return dist[dst];
     }
 };
