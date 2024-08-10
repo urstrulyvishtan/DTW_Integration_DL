27cd977dfd5341c2f42e2819102bcc15e754180e
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-10 19:38:24 -0400

    find cheapest price flights within K stops

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,28 @@
 #include<unordered_set>
 class Solution {
 public:
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        vector<vector<int>> visited(n, vector<int>(n, 0));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-        pq.push({grid[0][0], {0,0}});
-        visited[0][0] = 1;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        unordered_map<int, vector<pair<int, int>>> graph;
+        for(const auto& flight:flights){
+            int u = flight[0], v = flight[1], w = flight[2];
+            graph[u].emplace_back(v, w);
+        }
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        pq.emplace(0, src, 0);
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
         while(!pq.empty()){
-            auto[time, cell] = pq.top(); pq.pop();
-            int x = cell.first, y = cell.second;
-            if(x==n-1&&y==n-1)return time;
-            for(const auto& dir:directions){
-                int nx = x+dir[0], ny = y+dir[1];
-                if(nx>=0&&nx<n&&ny>=0&&ny<n&&!visited[nx][ny]){
-                    visited[nx][ny] = 1;
-                    pq.push({max(time, grid[nx][ny]), {nx, ny}});
+            auto[cost, city, stops] = pq.top(); pq.pop();
+            if(city == dst){
+                return cost;
+            }
+            if(stops <= k){
+                for(const auto& [next_city, price]:graph[city]){
+                    int new_cost = cost+price;
+                    if(new_cost<dist[next_city]||stops+1 <=k){
+                        dist[next_city] = new_cost;
+                        pq.emplace(new_cost, next_city, stops+1);
+                    }
                 }
             }
         }
