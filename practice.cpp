85bced9e2340fbea99674a6fa663fe8d5a046bbc
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-11 16:13:26 -0400

    target sum

--- practice.cpp
+++ practice.cpp
@@ -4,16 +4,31 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
+
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount+1, 0);
-        dp[0] = 1;
-        for(int coin:coins){
-            for(int j = coin; j<=amount; ++j){
-                dp[j] += dp[j-coin];
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        // If (sum + target) is odd or target is out of the possible sum range, return 0
+        if ((sum + target) % 2 != 0 || target > sum || target < -sum) {
+            return 0;
+        }
+        
+        int subsetSum = (sum + target) / 2;
+        
+        vector<int> dp(subsetSum + 1, 0);
+        dp[0] = 1;  // There's one way to reach sum 0: by choosing nothing
+        
+        for (int num : nums) {
+            for (int i = subsetSum; i >= num; --i) {
+                dp[i] += dp[i - num];
             }
         }
-        return dp[amount];
+        
+        return dp[subsetSum];
     }
 };
