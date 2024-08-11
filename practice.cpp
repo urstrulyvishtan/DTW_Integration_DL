d554b9fc192572ccb762f9d0869666c3bbd864d9
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-11 18:19:59 -0400

    edit distance

--- practice.cpp
+++ practice.cpp
@@ -6,19 +6,22 @@
 #include<unordered_set>
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int m = s.length();
-        int n = t.length();
-        vector<vector<unsigned long long>> dp(m+1, vector<unsigned long long>(n+1, 0));
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         for(int i = 0; i<=m; ++i){
-            dp[i][0] = 1;
+            dp[i][0] = i;
+        }
+        for(int j = 0; j<=n; ++j){
+            dp[0][j] = j;
         }
         for(int i = 1; i<=m; ++i){
             for(int j = 1; j<=n; ++j){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = min({dp[i-1][j] + 1, dp[i][j-1]+1, dp[i-1][j-1]+1});
                 }
             }
         }
