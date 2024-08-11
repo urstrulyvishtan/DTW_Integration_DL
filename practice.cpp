46b34eef0ee6214dadbf61df512a278b62869289
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-11 18:15:33 -0400

    distinct subsequence

--- practice.cpp
+++ practice.cpp
@@ -6,21 +6,20 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool isInterleave(string s1, string s2, string s3) {
-        int m = s1.length();
-        int n = s2.length();
-        if(m+n!=s3.length()) return false;
-        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
-        dp[0][0] = true;
-        for(int i = 1; i<=m; i++){
-            dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        vector<vector<unsigned long long>> dp(m+1, vector<unsigned long long>(n+1, 0));
+        for(int i = 0; i<=m; ++i){
+            dp[i][0] = 1;
         }
-        for(int j = 1; j<=n; j++){
-            dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
-        }
-        for(int i = 1; i<=m; i++){
-            for(int j = 1; j<=n; j++){
-                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) || (dp[i][j-1] && s2[j-1] == s3[i+j-1]);
+        for(int i = 1; i<=m; ++i){
+            for(int j = 1; j<=n; ++j){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
         return dp[m][n];
