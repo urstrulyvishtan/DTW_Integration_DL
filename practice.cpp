f8a7ee3ce7bb207bc75ce38a0d737efba723cbc0
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-11 16:20:32 -0400

    interleaving string

--- practice.cpp
+++ practice.cpp
@@ -4,31 +4,25 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
+    bool isInterleave(string s1, string s2, string s3) {
+        int m = s1.length();
+        int n = s2.length();
+        if(m+n!=s3.length()) return false;
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        dp[0][0] = true;
+        for(int i = 1; i<=m; i++){
+            dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
         }
-        
-        // If (sum + target) is odd or target is out of the possible sum range, return 0
-        if ((sum + target) % 2 != 0 || target > sum || target < -sum) {
-            return 0;
+        for(int j = 1; j<=n; j++){
+            dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
         }
-        
-        int subsetSum = (sum + target) / 2;
-        
-        vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1;  // There's one way to reach sum 0: by choosing nothing
-        
-        for (int num : nums) {
-            for (int i = subsetSum; i >= num; --i) {
-                dp[i] += dp[i - num];
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) || (dp[i][j-1] && s2[j-1] == s3[i+j-1]);
             }
         }
-        
-        return dp[subsetSum];
+        return dp[m][n];
     }
 };
