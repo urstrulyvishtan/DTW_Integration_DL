7cac04161da7d1fbe4564f2b9c294db8060fc974
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-12 16:54:25 -0400

    merging triplets to form a target triplet

--- practice.cpp
+++ practice.cpp
@@ -6,21 +6,17 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        if(hand.size()%groupSize != 0) return false;
-        unordered_map<int, int> count;
-        for(int card:hand){
-            count[card]++;
-        }
-        sort(hand.begin(), hand.end());
-        for(int card:hand){
-            if(count[card]>0){
-                for(int i = 0; i<groupSize; ++i){
-                    if(count[card+i]<=0) return false;
-                    count[card+i]--;
-                }
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        int maxX = 0, maxY = 0, maxZ = 0;
+        
+        for (const auto& triplet : triplets) {
+            if (triplet[0] <= target[0] && triplet[1] <= target[1] && triplet[2] <= target[2]) {
+                maxX = max(maxX, triplet[0]);
+                maxY = max(maxY, triplet[1]);
+                maxZ = max(maxZ, triplet[2]);
             }
         }
-        return true;
+        
+        return maxX == target[0] && maxY == target[1] && maxZ == target[2];
     }
 };
