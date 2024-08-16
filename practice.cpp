95627820d39ec891e0cce6c7c72ad0d1f37a55e0
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-16 10:53:01 -0400

    find the safest path in a grid max safeness factor

--- practice.cpp
+++ practice.cpp
@@ -6,39 +6,73 @@
 #include<unordered_set>
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int, double>>> graph(n);
-        for(int i = 0; i<edges.size(); ++i){
-            int u = edges[i][0], v=edges[i][1];
-            double prob = succProb[i];
-            graph[u].emplace_back(v, prob);
-            graph[v].emplace_back(u, prob);
-        }
-        priority_queue<pair<double, int>> pq;
-        pq.emplace(1.0, start);
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int nRows = grid.size();
+        int nCols = grid[0].size();
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        vector<double> probabilities(n, 0.0);
-        probabilities[start] = 1.0;
-
-        while(!pq.empty()){
-            double currProb = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            if(node == end){
-                return currProb;
-            }
-            for(auto& neighbor:graph[node]){
-                int nextNode = neighbor.first;
-                double edgeProb = neighbor.second;
-                double newProb = currProb * edgeProb;
-
-                if(newProb > probabilities[nextNode]){
-                    probabilities[nextNode] = newProb;
-                    pq.emplace(newProb, nextNode);
+        queue<pair<int, int>> thievesGrids;
+        for (int row = 0; row < nRows; row++) {
+            for (int col = 0; col < nCols; col++) {
+                if (grid[row][col] == 1) {
+                    thievesGrids.emplace(row, col);
+                    grid[row][col] = 0;  // Mark thief cell with 0
+                } else {
+                    grid[row][col] = -1; // Mark empty cell with -1
                 }
             }
         }
-        return 0.0;
+
+        bfs(grid, thievesGrids, nRows, nCols, directions);
+        return calculateSafeness(grid, nRows, nCols, directions);
+    }
+
+private:
+    void bfs(vector<vector<int>>& grid, queue<pair<int, int>>& thievesGrids, int nRows, int nCols, const vector<pair<int, int>>& directions) {
+        while (!thievesGrids.empty()) {
+            auto [row, col] = thievesGrids.front();
+            thievesGrids.pop();
+            int value = grid[row][col];
+
+            for (const auto& direction : directions) {
+                int newRow = row + direction.first;
+                int newCol = col + direction.second;
+                if (inBound(newRow, newCol, nRows, nCols) && grid[newRow][newCol] == -1) {
+                    grid[newRow][newCol] = value + 1;
+                    thievesGrids.emplace(newRow, newCol);
+                }
+            }
+        }
+    }
+
+    int calculateSafeness(vector<vector<int>>& grid, int nRows, int nCols, const vector<pair<int, int>>& directions) {
+        priority_queue<tuple<int, int, int>> priorityQueue;
+        priorityQueue.emplace(grid[0][0], 0, 0);
+        vector<vector<bool>> visited(nRows, vector<bool>(nCols, false));
+        visited[0][0] = true;
+
+        while (!priorityQueue.empty()) {
+            auto [safeness, row, col] = priorityQueue.top();
+            priorityQueue.pop();
+
+            if (row == nRows - 1 && col == nCols - 1) {
+                return safeness;
+            }
+
+            for (const auto& direction : directions) {
+                int newRow = row + direction.first;
+                int newCol = col + direction.second;
+                if (inBound(newRow, newCol, nRows, nCols) && !visited[newRow][newCol]) {
+                    visited[newRow][newCol] = true;
+                    priorityQueue.emplace(min(safeness, grid[newRow][newCol]), newRow, newCol);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    bool inBound(int row, int col, int nRows, int nCols) {
+        return 0 <= row && row < nRows && 0 <= col && col < nCols;
     }
 };
