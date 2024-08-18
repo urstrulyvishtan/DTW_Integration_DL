ac56d655921e1e478eaeecf55525117ce391e745
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-18 09:56:02 -0400

    data streams as disjoint intervals.
    `

--- practice.cpp
+++ practice.cpp
@@ -4,34 +4,41 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-class Solution {
+class SummaryRanges {
+private:
+    std::set<std::pair<int, int>> intervals;
 public:
-    vector<int> singleNumber(vector<int>& nums) {
-
-        long ans = 0;
-
-        for(auto &num: nums){
-
-            ans ^= num;
-            
-        }
-
-        int check = ans & (-ans);
-
-        int grpA = 0;
-        int grpB = 0;
-
-        for(auto &num: nums){
-
-            if(check & num){
-                grpA ^= num;
-            }else{
-                grpB ^= num;
-            }
-            
-        }
-
-        return {grpA,grpB};
+    SummaryRanges() {
         
     }
+    
+    void addNum(int val) {
+        auto it = intervals.lower_bound({val, val});
+        if(it != intervals.begin() && std::prev(it)->second + 1 >= val){
+            --it;
+        }
+        int start = val, end = val;
+        while(it != intervals.end() && it->first <= end+1){
+            start = std::min(start, it->first);
+            end = std::max(end, it->second);
+            it = intervals.erase(it);
+        }
+        intervals.insert({start, end});
+    }
+    std::vector<std::vector<int>> getIntervals(){
+        std::vector<std::vector<int>> result;
+        for(const auto& interval:intervals){
+            result.push_back({interval.first, interval.second});
+        }
+        return result;
+    }
+    
+    
 };
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges* obj = new SummaryRanges();
+ * obj->addNum(value);
+ * vector<vector<int>> param_2 = obj->getIntervals();
+ */
