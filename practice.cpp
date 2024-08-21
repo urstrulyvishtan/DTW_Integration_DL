fb7866ccdb0c8a40622b4eb6c16d20dbd882502b
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-21 08:02:51 -0400

    greatest common divisor

--- practice.cpp
+++ practice.cpp
@@ -4,77 +4,58 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-class UnionFind{
-public:
-    vector<int> parent, rank;
-    UnionFind(int n){
-        parent.resize(n);
-        rank.resize(n, 0);
-        iota(parent.begin(), parent.end(), 0);
-    }
-    int find(int u){
-        if(u!=parent[u]){
-            parent[u] = find(parent[u]);
-        }
-        return parent[u];
-    }
-    bool unionSets(int u, int v){
-        int rootU = find(u);
-        int rootV = find(v);
-        if(rootU == rootV) return false;
-        if(rank[rootU]>rank[rootV]){
-            parent[rootV] = rootU;
-        }else if(rank[rootU]<rank[rootV]){
-            parent[rootU] = rootV;
-        }else{
-            parent[rootV] = rootU;
-            rank[rootU]++;
-        }
-        return true;
-    }
-};
 class Solution {
 public:
-    vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
-        int m = edges.size();
-        for(int i = 0; i<m; ++i){
-            edges[i].push_back(i);
-        }
-        sort(edges.begin(), edges.end(), [](const auto& a, const auto& b){
-            return a[2]<b[2];
-        });
-        auto kruskalMST = [&](int n, vector<vector<int>>& edges, int excludeEdge = -1, int includeEdge = -1) -> int{
-            UnionFind uf(n);
-            int totalWeight = 0;
-            int edgesUsed = 0;
-            if(includeEdge!=-1){
-                uf.unionSets(edges[includeEdge][0], edges[includeEdge][1]);
-                totalWeight += edges[includeEdge][2];
-                edgesUsed++;
-            }
-            for(int i = 0; i<edges.size(); ++i){
-                if(i == excludeEdge) continue;
-                if(uf.unionSets(edges[i][0], edges[i][1])){
-                    totalWeight += edges[i][2];
-                    edgesUsed++;
-                    if(edgesUsed == n-1) break;
-                }
-            }
-            return edgesUsed == n-1?totalWeight:INT_MAX;
-        };
-        int originalMSTWeight = kruskalMST(n, edges);
-        vector<int> criticalEdges, pseudoCriticalEdges;
-        for(int i = 0; i<m; ++i){
-            int mstWeightWithoutEdge = kruskalMST(n, edges, i);
-            if(mstWeightWithoutEdge>originalMSTWeight){
-                criticalEdges.push_back(edges[i][3]);
-            }else{
-                int mstWeightWithEdge = kruskalMST(n, edges, -1, i);
-                if(mstWeightWithEdge == originalMSTWeight){
-                    pseudoCriticalEdges.push_back(edges[i][3]);
+    int get(vector<int> &parent, int a) {
+        return parent[a] = parent[a] == a ? a : get(parent, parent[a]);
+    }
+    void Union(vector<int> &parent, vector<int> &rank, int a, int b) {
+        a = get(parent, a);
+        b = get(parent, b);
+        if(a == b) return;
+        if(rank[a] < rank[b]) swap(a, b);
+        parent[b] = a;
+        rank[a] += rank[b];
+    }
+    vector<int> factorsCalculator(int n) {
+        vector<int> dp(n + 2);
+        iota(dp.begin(), dp.end(), 0);
+        for(int i = 2; i <= n; ++i) {
+            if(dp[i] == i) {
+                for(int j = i * 2; j <= n; j += i) {
+                    if(dp[j] == j) dp[j] = i;
                 }
             }
         }
-        return {criticalEdges, pseudoCriticalEdges};
+        return dp;
+    }
+    bool canTraverseAllPairs(vector<int>& nums) {
+        if(nums.size() == 1) return true;
+        int n = nums.size();
+        int maxElement = *max_element(nums.begin(), nums.end());
+        if(*min_element(nums.begin(), nums.end()) == 1) return false;
+        vector<int> factorArray = factorsCalculator(maxElement);
+
+        vector<int> parent(maxElement + 1);
+        iota(parent.begin(), parent.end(), 0);
+        ;
+        vector<int> rank(maxElement + 1, 1);
+        for(int i = 0; i < n; ++i) {
+            int x = nums[i];
+            while(x > 1) {
+                int p = factorArray[x];
+                Union(parent, rank, p, nums[i]);
+                while(x % p == 0) {
+                    x = x / p;
+                }
+            }
+        }
+
+        int p = get(parent, nums[0]);
+        for(int i = 1; i < n; ++i) {
+            if(get(parent, nums[i]) != p) return false;
+        }
+
+        return true;
     }
 };
