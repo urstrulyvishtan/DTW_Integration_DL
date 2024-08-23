a49565650c83c69359b10c985350c01fa300129c
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-23 07:28:24 -0400

    reverse string

--- practice.cpp
+++ practice.cpp
@@ -6,29 +6,12 @@
 #include<unordered_set>
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) {
-        // Initialize an empty result string
-        string result = "";
-        
-        // Two pointers to traverse both strings
-        int i = 0, j = 0;
-        
-        // Merge the strings alternately
-        while (i < word1.size() && j < word2.size()) {
-            result += word1[i++];
-            result += word2[j++];
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while(left<right){
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
-        
-        // If there are remaining characters in word1, append them
-        while (i < word1.size()) {
-            result += word1[i++];
-        }
-        
-        // If there are remaining characters in word2, append them
-        while (j < word2.size()) {
-            result += word2[j++];
-        }
-        
-        return result;
     }
 };
