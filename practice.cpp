1fd768408b28df96ef5836ebc6fedd3c989a8681
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-23 07:17:57 -0400

    valid palindrome 2

--- practice.cpp
+++ practice.cpp
@@ -6,15 +6,24 @@
 #include<unordered_set>
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";
-        std::string prefix = strs[0];
-        for(int i = 1; i<strs.size(); i++){
-            while(strs[i].find(prefix)!=0){
-                prefix = prefix.substr(0, prefix.size() - 1);
-                if(prefix.empty()) return "";
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return isPalindrome(s, left+1, right)||isPalindrome(s, left, right-1);
             }
+            left++;
+            right--;
         }
-        return prefix;
+        return true;
+    }
+private:
+    bool isPalindrome(const std::string& s, int left, int right){
+        while(left<right){
+            if(s[left]!=s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 };
