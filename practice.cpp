ebfce7f213553b0f6b878dadd171af9a55501b35
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-23 07:21:45 -0400

    min difference between k scores

--- practice.cpp
+++ practice.cpp
@@ -6,24 +6,13 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool validPalindrome(string s) {
-        int left = 0, right = s.size()-1;
-        while(left<right){
-            if(s[left]!=s[right]){
-                return isPalindrome(s, left+1, right)||isPalindrome(s, left, right-1);
-            }
-            left++;
-            right--;
+    int minimumDifference(vector<int>& scores, int k) {
+        std::sort(scores.begin(), scores.end());
+        int minDiff = INT_MAX;
+        for(int i = 0; i<=scores.size()-k; ++i){
+            int diff = scores[i+k-1]-scores[i];
+            minDiff = std::min(minDiff, diff);
         }
-        return true;
-    }
-private:
-    bool isPalindrome(const std::string& s, int left, int right){
-        while(left<right){
-            if(s[left]!=s[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
+        return minDiff;
     }
 };
