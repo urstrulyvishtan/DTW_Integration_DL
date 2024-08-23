777c2ee369f694ce909449a13991abd6fa8372f3
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-23 07:25:53 -0400

    merge alternately

--- practice.cpp
+++ practice.cpp
@@ -6,13 +6,29 @@
 #include<unordered_set>
 class Solution {
 public:
-    int minimumDifference(vector<int>& scores, int k) {
-        std::sort(scores.begin(), scores.end());
-        int minDiff = INT_MAX;
-        for(int i = 0; i<=scores.size()-k; ++i){
-            int diff = scores[i+k-1]-scores[i];
-            minDiff = std::min(minDiff, diff);
+    string mergeAlternately(string word1, string word2) {
+        // Initialize an empty result string
+        string result = "";
+        
+        // Two pointers to traverse both strings
+        int i = 0, j = 0;
+        
+        // Merge the strings alternately
+        while (i < word1.size() && j < word2.size()) {
+            result += word1[i++];
+            result += word2[j++];
         }
-        return minDiff;
+        
+        // If there are remaining characters in word1, append them
+        while (i < word1.size()) {
+            result += word1[i++];
+        }
+        
+        // If there are remaining characters in word2, append them
+        while (j < word2.size()) {
+            result += word2[j++];
+        }
+        
+        return result;
     }
 };
