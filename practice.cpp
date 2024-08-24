63c3d70a02569a63e40f18b168b9a5e1f49f6ba9
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-24 12:35:49 -0400

    middle of linked list

--- practice.cpp
+++ practice.cpp
@@ -16,18 +16,13 @@
  */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(head == nullptr || head->next == nullptr){
-            return head;
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* curr = head;
-        while(curr!=nullptr && curr->next != nullptr){
-            if(curr->val == curr->next->val){
-                curr->next = curr->next->next;
-            }else{
-                curr = curr->next;
-            }
-        }
-        return head;
+        return slow;
     }
 };
