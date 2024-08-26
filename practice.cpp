49f239a3e9a046d1d14da00dcc809566c0b10a1f
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-26 07:33:55 -0400

    guess number higher or lower

--- practice.cpp
+++ practice.cpp
@@ -4,20 +4,30 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
+    int guessNumber(int n) {
+        int left = 1, right = n;
         while(left<=right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target){
+            int mid = left+(right - left)/2;
+            int res = guess(mid);
+            if(res == 0){
                 return mid;
-            }else if(nums[mid]<target){
-                left = mid + 1;
+            }else if(res == 1){
+                left = mid+1;
             }else{
                 right = mid - 1;
             }
         }
-        return left;
+        return -1;
     }
 };
