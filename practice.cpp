90fa074f42ada9710531b5afed74138da7436b74
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-27 11:14:48 -0400

    fruit into basket

--- practice.cpp
+++ practice.cpp
@@ -6,23 +6,20 @@
 #include<unordered_set>
 class Solution {
 public:
-    int mySqrt(int x) {
-        if(x<2) return x;
-        long low = 1;
-        long high = x;
-        int result = 0;
-        while(low<=high){
-            long mid = low+(high-low)/2;
-            long square = mid*mid;
-            if(square == x){
-                return mid;
-            }else if(square<x){
-                result = mid;
-                low = mid + 1;
-            }else{
-                high = mid - 1;
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> fruitCount;
+        int left = 0, maxFruits = 0;
+        for(int right = 0; right<fruits.size(); ++right){
+            fruitCount[fruits[right]]++;
+            while(fruitCount.size()>2){
+                fruitCount[fruits[left]]--;
+                if(fruitCount[fruits[left]] == 0){
+                    fruitCount.erase(fruits[left]);
+                }
+                left++;
             }
+            maxFruits = max(maxFruits, right-left+1);
         }
-        return result;
+        return maxFruits;
     }
 };
