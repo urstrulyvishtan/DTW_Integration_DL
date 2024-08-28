a7ed086de8fcb9b626babe1a490eebdd9e8fadc4
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-28 11:51:29 -0400

    inorder tree traversal

--- practice.cpp
+++ practice.cpp
@@ -4,20 +4,29 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        long long total = 0;
-        int left = 0, result = 1;
-        for(int right = 0; right<nums.size(); ++right){
-            total+=nums[right];
-            while((long long)nums[right] * (right-left+1)>total+k){
-                total -= nums[left];
-                left++;
-            }
-            result = max(result, right-left+1);
-        }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorderHelper(root, result);
         return result;
+        }
+private:
+    void inorderHelper(TreeNode* root, vector<int>& result){
+        if(root == nullptr) return;
+        inorderHelper(root->left, result);
+        result.push_back(root->val);
+        inorderHelper(root->right, result);
     }
 };
