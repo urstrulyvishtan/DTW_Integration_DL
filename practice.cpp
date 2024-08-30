afdbaa48cc7dbe23a1d5c258570f398b5f65d93d
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-30 11:41:34 -0400

    pascal triangle

--- practice.cpp
+++ practice.cpp
@@ -6,7 +6,20 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        return n>0 &&(n&(n-1)) == 0;
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+        if(numRows == 0){
+            return triangle;
+        }
+        triangle.push_back({1});
+        for(int i = 1; i<numRows; ++i)
+        {
+            vector<int> row(i+1, 1);
+            for(int j = 1; j<i; ++j){
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j];
+            }
+            triangle.push_back(row);
+        }
+        return triangle;
     }
 };
