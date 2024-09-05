52230d7566b276cf998719faff1c14c16b2a8cab
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-05 19:59:16 -0400

    median of two sorted array

--- practice.cpp
+++ practice.cpp
@@ -6,20 +6,37 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> result(n, 0);
-        stack<int> stk;
-
-        for (int i = 0; i < n; ++i) {
-            while (!stk.empty() && temperatures[i] > temperatures[stk.top()]) {
-                int idx = stk.top();
-                stk.pop();
-                result[idx] = i - idx;
-            }
-            stk.push(i);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        return result;
+        int m = nums1.size();
+        int n = nums2.size();
+        int low = 0, high = m;
+        
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+            int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+
+            if (maxX <= minY && maxY <= minX) {
+                if ((m + n) % 2 == 0) {
+                    return (double)(max(maxX, maxY) + min(minX, minY)) / 2;
+                } else {
+                    return (double)max(maxX, maxY);
+                }
+            } else if (maxX > minY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+
+        throw invalid_argument("Input arrays are not sorted");
     }
 };
