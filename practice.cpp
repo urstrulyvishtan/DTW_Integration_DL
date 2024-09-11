43b18110339f53ebbcc286e3dc7d547ce2306cef
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-11 09:32:35 -0400

    maximum sum circular subarray

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,26 @@
 #include<unordered_set>
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int n=cardPoints.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
-        sum+=cardPoints[i];
-        int m=n-k;
-        int ans=0,final_ans=INT_MAX;
-        int start=0;
-        for(int i=0;i<n;i++){
-            ans+=cardPoints[i];
-            if(i-start+1==m){
-                final_ans=min(final_ans,ans);
-                ans-=cardPoints[start];
-                start++;
-            }
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int pos = kadane(nums, 1), neg = sum + kadane(nums, -1);
+        if(neg > pos && neg == 0) {
+            for(int num: nums)
+                if(num == 0)
+                    return 0;
+            return pos;
         }
-        return final_ans==INT_MAX ? sum : sum-final_ans;
+        return max(pos, neg);
+    }
+    int kadane(vector<int>& nums, int sign) {
+        int global, local;
+        global = sign * nums[0];
+        local = 0;
+        for(int num: nums) {
+            local += sign * num;
+            global = max(global, local);
+            local = max(local, 0);
+        }
+        return global;
     }
 };
