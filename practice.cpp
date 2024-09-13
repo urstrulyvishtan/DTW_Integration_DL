233c22e42f2be4d55b74782d77508fb812928076
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-13 16:52:52 -0400

    remove nth node from ll

--- practice.cpp
+++ practice.cpp
@@ -14,46 +14,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
-        if (lists.empty())
-            return nullptr;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* res = new ListNode(0, head);
+        ListNode* dummy = res;
 
-        auto cmp = [](ListNode* a, ListNode* b) {
-            return a->val > b->val; // Min-heap based on node value
-        };
-
-        std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(cmp)>
-            minHeap(cmp);
-
-        // Add the first node of each list to the min-heap
-        for (ListNode* list : lists) {
-            if (list) {
-                minHeap.push(list);
-            }
+        for (int i = 0; i < n; i++) {
+            head = head->next;
         }
 
-        ListNode dummy;
-        ListNode* current = &dummy;
-
-        // While there are nodes in the min-heap
-        while (!minHeap.empty()) {
-            // Get the smallest node
-            ListNode* smallestNode = minHeap.top();
-            minHeap.pop();
-            current->next = smallestNode; // Attach it to the merged list
-            current =
-                current->next; // Move to the next position in the merged list
-
-            // If there are more nodes in the list, add the next node to the
-            // min-heap
-            if (smallestNode->next) {
-                minHeap.push(smallestNode->next);
-            }
+        while (head != nullptr) {
+            head = head->next;
+            dummy = dummy->next;
         }
 
-        return dummy.next; // Return the head of the merged list
+        dummy->next = dummy->next->next;
+
+        return res->next;        
     }
 };
