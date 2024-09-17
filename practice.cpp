371071d1e20b415b705841d646fb866ef09383d5
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-17 16:49:10 -0400

    min window substring

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,48 @@
 #include<unordered_set>
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> st; // create an empty stack to store opening brackets
-        for (char c : s) { // loop through each character in the string
-            if (c == '(' || c == '{' || c == '[') { // if the character is an opening bracket
-                st.push(c); // push it onto the stack
-            } else { // if the character is a closing bracket
-                if (st.empty() || // if the stack is empty or 
-                    (c == ')' && st.top() != '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
-                    (c == '}' && st.top() != '{') ||
-                    (c == ']' && st.top() != '[')) {
-                    return false; // the string is not valid, so return false
+    string minWindow(string s, string t) {
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        unordered_map<char, int> charCount;
+        for (char ch : t) {
+            charCount[ch]++;
+        }
+
+        int targetCharsRemaining = t.length();
+        int minWindow[2] = {0, INT_MAX};
+        int startIndex = 0;
+
+        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
+            char ch = s[endIndex];
+            if (charCount.find(ch) != charCount.end() && charCount[ch] > 0) {
+                targetCharsRemaining--;
+            }
+            charCount[ch]--;
+
+            if (targetCharsRemaining == 0) {
+                while (true) {
+                    char charAtStart = s[startIndex];
+                    if (charCount.find(charAtStart) != charCount.end() && charCount[charAtStart] == 0) {
+                        break;
+                    }
+                    charCount[charAtStart]++;
+                    startIndex++;
                 }
-                st.pop(); // otherwise, pop the opening bracket from the stack
+
+                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
+                    minWindow[0] = startIndex;
+                    minWindow[1] = endIndex;
+                }
+
+                charCount[s[startIndex]]++;
+                targetCharsRemaining++;
+                startIndex++;
             }
         }
-        return st.empty(); // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-                           // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+
+        return minWindow[1] >= s.length() ? "" : s.substr(minWindow[0], minWindow[1] - minWindow[0] + 1);        
     }
 };
