6a8c923146722aaf9b474338765cc1107b5558b1
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-20 19:34:54 -0400

    best time to sell stock

--- practice.cpp
+++ practice.cpp
@@ -6,48 +6,18 @@
 #include<unordered_set>
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        if (s.length() < t.length()) {
-            return "";
-        }
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
 
-        unordered_map<char, int> charCount;
-        for (char ch : t) {
-            charCount[ch]++;
-        }
-
-        int targetCharsRemaining = t.length();
-        int minWindow[2] = {0, INT_MAX};
-        int startIndex = 0;
-
-        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
-            char ch = s[endIndex];
-            if (charCount.find(ch) != charCount.end() && charCount[ch] > 0) {
-                targetCharsRemaining--;
+        for (int i = 1; i < prices.size(); i++) {
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
             }
-            charCount[ch]--;
 
-            if (targetCharsRemaining == 0) {
-                while (true) {
-                    char charAtStart = s[startIndex];
-                    if (charCount.find(charAtStart) != charCount.end() && charCount[charAtStart] == 0) {
-                        break;
-                    }
-                    charCount[charAtStart]++;
-                    startIndex++;
-                }
-
-                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
-                    minWindow[0] = startIndex;
-                    minWindow[1] = endIndex;
-                }
-
-                charCount[s[startIndex]]++;
-                targetCharsRemaining++;
-                startIndex++;
-            }
+            profit = max(profit, prices[i] - buyPrice);
         }
 
-        return minWindow[1] >= s.length() ? "" : s.substr(minWindow[0], minWindow[1] - minWindow[0] + 1);        
+        return profit;        
     }
 };
