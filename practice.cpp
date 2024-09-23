f84064ff4a62b3a573e824d48d639effc6ee7d0e
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-23 16:37:32 -0400

    max depth

--- practice.cpp
+++ practice.cpp
@@ -4,41 +4,23 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-class MedianFinder {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-    priority_queue<int>left; // max heap
-    priority_queue<int,vector<int>,greater<int>>right; // min heap
-    MedianFinder() { // default constructor
-        
-    }
-    
-    void addNum(int num) { // O(logn)
-        if(left.size() == 0 || num < left.top()) left.push(num);
-        else right.push(num);
-        // if left has 2 more element than right
-        if(left.size() > right.size()+1){
-            right.push(left.top());
-            left.pop();
-        }
-        // if right has 2 more element than left
-        if(right.size() > left.size()+1){
-            left.push(right.top());
-            right.pop();
-        }
-    }
-    
-    double findMedian() { // O(1)
-        if(left.size() == right.size()) return (left.top()+right.top())/2.0;
-        else {
-            if(left.size() > right.size()) return left.top();
-            else return right.top();
-        }
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
