a7a9bbf5f3a4617b0e68ded81ce70f7c50dfb48a
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-29 18:22:52 -0400

    spiral matrix

--- practice.cpp
+++ practice.cpp
@@ -6,16 +6,46 @@
 #include<unordered_set>
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        
+        if (matrix.empty()) return result;
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0, right = n - 1;
+        int top = 0, bottom = m - 1;
+
+        while (left <= right && top <= bottom) {
+            // Traverse from left to right along the top boundary
+            for (int i = left; i <= right; ++i) {
+                result.push_back(matrix[top][i]);
+            }
+            top++;
+            
+            // Traverse from top to bottom along the right boundary
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            // Traverse from right to left along the bottom boundary
+            if (top <= bottom) {
+                for (int i = right; i >= left; --i) {
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            // Traverse from bottom to top along the left boundary
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
         }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+        
+        return result;
     }
 };
