f3a2420fc0e79534537bfa617aa4723dec78d660
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-30 19:24:22 -0400

    missing number

--- practice.cpp
+++ practice.cpp
@@ -6,46 +6,13 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        
-        if (matrix.empty()) return result;
-        
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int left = 0, right = n - 1;
-        int top = 0, bottom = m - 1;
-
-        while (left <= right && top <= bottom) {
-            // Traverse from left to right along the top boundary
-            for (int i = left; i <= right; ++i) {
-                result.push_back(matrix[top][i]);
-            }
-            top++;
-            
-            // Traverse from top to bottom along the right boundary
-            for (int i = top; i <= bottom; ++i) {
-                result.push_back(matrix[i][right]);
-            }
-            right--;
-            
-            // Traverse from right to left along the bottom boundary
-            if (top <= bottom) {
-                for (int i = right; i >= left; --i) {
-                    result.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            
-            // Traverse from bottom to top along the left boundary
-            if (left <= right) {
-                for (int i = bottom; i >= top; --i) {
-                    result.push_back(matrix[i][left]);
-                }
-                left++;
-            }
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n*(n+1)/2;
+        int actualSum = 0;
+        for(int num:nums){
+            actualSum+=num;
         }
-        
-        return result;
+        return expectedSum - actualSum;
     }
 };
