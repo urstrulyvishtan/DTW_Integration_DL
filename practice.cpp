32c463cf1dd8fdadae35df68147d9569aa3b9579
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-10-03 19:31:11 -0400

    meta-ispalindrome

--- practice.cpp
+++ practice.cpp
@@ -6,37 +6,24 @@
 #include<unordered_set>
 class Solution {
 public:
-    string minRemoveToMakeValid(string s) {
-        vector<int> pl;
-        vector<int> pr;
-        for(int i = 0; i<s.length(); i++){
-            if(s[i]=='('){
-                pl.push_back(i);
-            }else if(s[i] == ')'){
-                pr.push_back(i);
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return isPalindrome(s, left+1, right)||isPalindrome(s, left, right-1);
             }
+            left++;
+            right--;
         }
-        int p1=0, r1=0, c=0;
-        while(p1<pl.size()&&r1<pr.size()){
-            if(pl[p1]>pr[r1]){
-                s.erase(s.begin()+pr[r1]-c);
-                c++;
-                r1++;
-            }else{
-                p1++;
-                r1++;
-            }
+        return true;
+    }
+private:
+    bool isPalindrome(const std::string& s, int left, int right){
+        while(left<right){
+            if(s[left]!=s[right]) return false;
+            left++;
+            right--;
         }
-        while(p1<pl.size()){
-            s.erase(s.begin()+pl[p1]-c);
-            c++;
-            p1++;
-        }
-        while(r1<pr.size()){
-            s.erase(s.begin()+pr[r1]-c);
-            c++;
-            r1++;
-        }
-        return s;
+        return true;
     }
 };
