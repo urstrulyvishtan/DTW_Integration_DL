39abc04a43b1b00622c1f67619c955ab281a6d0e
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-10-03 19:29:21 -0400

    min remove to make valid string

--- practice.cpp
+++ practice.cpp
@@ -6,37 +6,37 @@
 #include<unordered_set>
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        if (obstacleGrid[m - 1][n - 1] == 1) {
-            return 0;
-        }
-        dp[m - 1][n - 1] = 1;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (i == m - 1 && j == n - 1) {
-                    continue;
-                }
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } 
-                else {
-                    long long ans1, ans2;
-                    if (i < m - 1)
-                        ans1 = dp[i + 1][j];
-                    else
-                        ans1 = 0;
-                    if (j < n - 1)
-                        ans2 = dp[i][j + 1];
-                    else
-                        ans2 = 0;
-                    dp[i][j] = ans1 + ans2;
-                }
+    string minRemoveToMakeValid(string s) {
+        vector<int> pl;
+        vector<int> pr;
+        for(int i = 0; i<s.length(); i++){
+            if(s[i]=='('){
+                pl.push_back(i);
+            }else if(s[i] == ')'){
+                pr.push_back(i);
             }
         }
-        return dp[0][0];
+        int p1=0, r1=0, c=0;
+        while(p1<pl.size()&&r1<pr.size()){
+            if(pl[p1]>pr[r1]){
+                s.erase(s.begin()+pr[r1]-c);
+                c++;
+                r1++;
+            }else{
+                p1++;
+                r1++;
+            }
+        }
+        while(p1<pl.size()){
+            s.erase(s.begin()+pl[p1]-c);
+            c++;
+            p1++;
+        }
+        while(r1<pr.size()){
+            s.erase(s.begin()+pr[r1]-c);
+            c++;
+            r1++;
+        }
+        return s;
     }
 };
