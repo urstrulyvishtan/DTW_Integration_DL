170bf8d3bd8a08b9f77296db2b313af5b78879a2
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-10-06 18:35:17 -0400

    kth largest element

--- practice.cpp
+++ practice.cpp
@@ -6,27 +6,22 @@
 #include<unordered_set>
 class Solution {
 public:
-vector<int> g;
-    Solution(vector<int>& w) {
-        g.resize(w.size());
-        for(int i = 0; i<w.size(); i++){
-            if(i == 0){
-                g[i] = w[i];
-                continue;
-            } g[i] = g[i-1]+w[i];
+    int findKthLargest(vector<int>& nums, int k) {
+        k = nums.size() - k;
+        
+        return quickSelect(0, nums.size()-1);
+    }
+    void quickSelect(l, r){
+            int pivot = nums[r], p = l;
+            for(int i = 0; i<r; i++){
+                if(nums[i]<=pivot){
+                    swap(nums[p], nums[i]);
+                    p++;
+                }
+                swap(nums[p], nums[r]);
+                if(p>k) return quickSort(l, p-1);
+                elif(p<k) return quickSort(p+1, r);
+                else return nums[p];
+            }
         }
-    }
-    
-    int pickIndex() {
-        // random number from 0, w.length-1
-        int rand_i = rand()%g.back();
-        int index = upper_bound(g.begin(), g.end(), rand_i)-g.begin();
-        return index;
-    }
 };
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution* obj = new Solution(w);
- * int param_1 = obj->pickIndex();
- */
