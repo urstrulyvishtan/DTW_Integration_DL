d7d9a3d6e8c8e543d442b41b9ff92a4782fda713
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-10-07 11:23:00 -0400

    kth largest in array

--- practice.cpp
+++ practice.cpp
@@ -7,21 +7,13 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        k = nums.size() - k;
-        
-        return quickSelect(0, nums.size()-1);
-    }
-    void quickSelect(l, r){
-            int pivot = nums[r], p = l;
-            for(int i = 0; i<r; i++){
-                if(nums[i]<=pivot){
-                    swap(nums[p], nums[i]);
-                    p++;
-                }
-                swap(nums[p], nums[r]);
-                if(p>k) return quickSort(l, p-1);
-                elif(p<k) return quickSort(p+1, r);
-                else return nums[p];
+        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+        for(int num : nums){
+            minHeap.push(num);
+            if(minHeap.size()>k){
+                minHeap.pop();
             }
         }
+        return minHeap.top();
+    }
 };
