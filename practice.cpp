61d2198a313c0203ce82ebda7e53d0b8497fe872
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-10-08 16:02:39 -0400

    kth largest in a stream

--- practice.cpp
+++ practice.cpp
@@ -4,16 +4,38 @@
 #include <algorithm>
 #include<unordered_map>
 #include<unordered_set>
-class Solution {
+class KthLargest {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
-        for(int num : nums){
-            minHeap.push(num);
-            if(minHeap.size()>k){
-                minHeap.pop();
-            }
+
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int K;
+    
+    KthLargest(int k, vector<int>& nums) {
+
+        K=k;
+
+        for(auto &num:nums){
+
+            pq.push(num);
+
+            if(pq.size() > k) pq.pop();
+            
         }
-        return minHeap.top();
+    }
+    
+    int add(int val) {
+
+        pq.push(val);
+
+        if(pq.size() > K) pq.pop();
+
+        return pq.top();
+        
     }
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
