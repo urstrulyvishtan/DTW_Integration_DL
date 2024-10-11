ec334bae44cf7e61bd90cc058d90e7b8a79c7d75
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-10-11 19:41:26 -0400

    valid palindrome

--- practice.cpp
+++ practice.cpp
@@ -6,22 +6,18 @@
 #include<unordered_set>
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, vector<int>>> maxHeap;
-        for(const auto& point:points){
-            int x = point[0];
-            int y = point[1];
-            int distance = x*x + y*y;
-            maxHeap.push({distance, point});
-            if(maxHeap.size()>k){
-                maxHeap.pop();
-            }
-        }
-        vector<vector<int>> result;
-        while(!maxHeap.empty()){
-            result.push_back(maxHeap.top().second);
-            maxHeap.pop();
-        }
-        return result;
-    }
+    bool isPalindrome(string s) {
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
